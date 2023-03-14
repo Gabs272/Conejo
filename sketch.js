a33510abc060632ff5bf2ce229d8e02dbd56af98
Johanalopez0922 <121140087+Johanalopez0922@users.noreply.github.com>
2023-03-14 16:36:14 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+var stand1,stand2;
+var ball;
+var slingShot;
+var fruit;
+
+function preload(){
+  backgroundImg = loadImage("background.png");
+ 
+}
+
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  ground = new Ground();
+  
+ 
+}
+
+function draw() {
+  background(backgroundImg); 
+ 
+  //Engine.update(engine);
+  //text(mouseX + ',' + mouseY, 10, 15);
+  
+  ground.display();
+  
+}
+
+
+function mouseDragged(){
+  
+}
+
+function mouseReleased(){
+ 
+
+}
